bd617b1a3af316df892e2fc0e8e69135af0abe88
Dravel IMBOLO <dravelameguste@gmail.com>
2026-04-06 19:37:32 +0200

    chore(config): split settings dev/prod and configure urls
    - SQLite for development, PostgreSQL for production
    - ALLOWED_HOSTS from environment variable
    - Root urls configured

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Auth email/password + social(allauth)
+    path('auth/', include('allauth.urls')),
+
+    # Apps locales
+    path('', include('apps.accounts.uls', namespace="accounts")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
